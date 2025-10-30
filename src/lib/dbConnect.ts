ff9c247e30d87951d1aa262116e6c25bc32ce132
ju3tin <ju3tin@hotmail.co.uk>
2025-10-30 22:41:14 +0000

    sdf

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -6,6 +6,10 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
+declare global {
+  var mongoose: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+}
+
 let cached = global.mongoose;
 
 if (!cached) {
