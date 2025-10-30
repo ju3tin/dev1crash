04073b35b4a28c98de153b91cab1872129ad9217
ju3tin <ju3tin@hotmail.co.uk>
2025-10-30 23:51:40 +0000

    sdfsdfxsd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,9 @@ const config: Config = {
     darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
