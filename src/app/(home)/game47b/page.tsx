687d008e1fbdd652786207cd5b8faf0aa2b35e1c
ju3tin <ju3tin@hotmail.co.uk>
2025-11-14 13:22:26 +0000

    dfg

--- src/app/(home)/game47b/page.tsx
+++ src/app/(home)/game47b/page.tsx
@@ -759,7 +759,7 @@ useEffect(() => {
 
      
       <div className="w-full">
-      <Navbar />
+     
       
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
         {!isMobile && <div className="lg:col-span-3 h-[500px]">
