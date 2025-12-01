7e846bcf1841688748da6473b805cce3075e3541
ju3tin <ju3tin@hotmail.co.uk>
2025-12-01 12:55:59 +0000

    dfgdfg

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,22 +1,61 @@
 "use client";
 export default function Roadmap() {
     return (
+        <>
 <div id="container12" className="container default pt-4">
     <div className="wrapper1">
         <div className="inner">
       <h1 className='text-3xl font-semibold capitalize'>About</h1>
       <h2 id="text09" className="style4" style={{opacity: 1, transform: "none"}}>CHIPPY ON SOLANA IS THE FRIENDLY FISH AND CHIPS YOU ALWAYS NEEDED. IT IS THE BEST COMBO IN ALL THE SOL SEA. JOIN OUR COMMUNITY TO FIND OTHER FISHY FRIENDS!</h2>
       <p>
-      <ul>
-      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/aa7bc757.png" alt="Untitled"/></span></a></li>
-      <li><a href="https://t.me/fishnchipssol" className="thumbnail n01"><span className="frame"><img src="/images/c7f40097.png" alt="Untitled"/></span></a></li>
-      <li><a href="https://solscan.io/token/Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01"><span className="frame"><img src="/images/43bb0dd1.png" alt="Untitled"/></span></a></li>
-      <li><a href="https://www.dextools.io/app/en/token/chippy?t=1764592782335" className="thumbnail n01"><span className="frame"><img src="/images/6cb17d91.png" alt="Untitled"/></span></a></li>
-      <li><a href="https://birdeye.so/solana/token/Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01"><span className="frame"><img src="/images/61a9adf4.png" alt="Untitled"/></span></a></li>
-      <li><a href="https://jup.ag/swap/SOL-Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01"><span className="frame"><img src="/images/af144825.png" alt="Untitled"/></span></a></li>
+      <ul className="socialList">
+        <li>
+          <a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01">
+            <span className="frame"><img src="/images/aa7bc757.png" alt="Untitled" /></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/fishnchipssol" className="thumbnail n01">
+            <span className="frame"><img src="/images/c7f40097.png" alt="Untitled" /></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://solscan.io/token/Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01">
+            <span className="frame"><img src="/images/43bb0dd1.png" alt="Untitled" /></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.dextools.io/app/en/token/chippy?t=1764592782335" className="thumbnail n01">
+            <span className="frame"><img src="/images/6cb17d91.png" alt="Untitled" /></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://birdeye.so/solana/token/Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01">
+            <span className="frame"><img src="/images/61a9adf4.png" alt="Untitled" /></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://jup.ag/swap/SOL-Bz7Nx1F3Mti1BVS7ZAVDLSKGEaejufxvX2DPdjpf8PqT" className="thumbnail n01">
+            <span className="frame"><img src="/images/af144825.png" alt="Untitled" /></span>
+          </a>
+        </li>
       </ul>
        </p>
         </div>
     </div>
 </div>
+<style jsx>{`
+        .socialList {
+          display: flex;
+          gap: 12px;
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+
+        .socialList li {
+          display: flex;
+        }
+      `}</style>
+      </>
 )}
