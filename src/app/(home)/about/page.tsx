82c4babe731c202fd0b6f659671be51dc7dbc0e8
ju3tin <ju3tin@hotmail.co.uk>
2025-12-01 12:36:22 +0000

    sdfds

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -5,19 +5,17 @@ export default function Roadmap() {
     <div className="wrapper1">
         <div className="inner">
       <h1 className='text-3xl font-semibold capitalize'>About</h1>
+      <h2 id="text09" className="style4" style={{opacity: 1, transform: "none"}}>CHIPPY ON SOLANA IS THE FRIENDLY FISH AND CHIPS YOU ALWAYS NEEDED. IT IS THE BEST COMBO IN ALL THE SOL SEA. JOIN OUR COMMUNITY TO FIND OTHER FISHY FRIENDS!</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sapiente
-        tenetur animi excepturi tempora aperiam, fugit in architecto, iusto
-        magni placeat obcaecati voluptatem soluta labore similique reiciendis
-        non a harum autem id provident omnis corrupti consequuntur velit?
-        Architecto repellat maiores quam ducimus nobis veritatis delectus
-        molestias, inventore corrupti modi minima commodi quos fugit odit
-        ratione tempore iure ipsam soluta harum sequi quas excepturi! Tenetur
-        laboriosam laborum quibusdam enim dicta adipisci, nostrum ipsa provident
-        odio nemo dolore optio, ad doloremque ducimus praesentium iure neque
-        voluptas. Aliquid eveniet incidunt facere qui itaque reiciendis labore
-        est, facilis, iusto ex iste. Eos, ducimus iure!
-      </p>
+      <ul>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/aa7bc757.png" alt="Untitled"/></span></a></li>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/c7f40097.png" alt="Untitled"/></span></a></li>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/43bb0dd1.png" alt="Untitled"/></span></a></li>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/6cb17d91.png" alt="Untitled"/></span></a></li>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/61a9adf4.png" alt="Untitled"/></span></a></li>
+      <li><a href="https://twitter.com/FishnChipsSOL" className="thumbnail n01"><span className="frame"><img src="/images/af144825.png" alt="Untitled"/></span></a></li>
+      </ul>
+       </p>
         </div>
     </div>
 </div>
