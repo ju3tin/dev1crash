7d400d285dbdec76bd30a90560a041d5069dfcdd
ju3tin <ju3tin@hotmail.co.uk>
2025-12-03 16:44:50 +0000

    sdf

--- src/app/(home)/stake/page.tsx
+++ src/app/(home)/stake/page.tsx
@@ -136,7 +136,7 @@ const getIDL = (): StakingProgramIDL => {
 }
 
 // Your actual program ID
-const programId = new PublicKey(process.env.PROGRAM_ID6 ?? '')
+const programId = new PublicKey(process.env.PROGRAM_ID6 || '')
 
 // Type the program properly
 type StakingProgram = Program<StakingProgramIDL>
